c7c4d3600283dbb6ff866ae520df846c8db1b8bc
michaelmartins09 <michaelmartins096@gmail.com>
2021-04-29 12:45:45 -0300

    reactor: change layout and data from generate graph

--- scripts/generate_graph.js
+++ scripts/generate_graph.js
@@ -1,4 +1,5 @@
 const plotLib = require('nodeplotlib')
+const Layout = require('nodeplotlib')
 const { lab } = require("./execute_lab")
 
 const generateGraph = () => {
@@ -6,20 +7,45 @@ const generateGraph = () => {
 
   const data = [
     {
-      y: [bruteForce.experimento1.time, bruteForce.experimento2.time, bruteForce.experimento3.time], 
-      x: [bruteForce.experimento1.charge, bruteForce.experimento2.charge, bruteForce.experimento3.charge],
+      y: [
+        bruteForce.experimento1.time, 
+        bruteForce.experimento2.time, 
+        bruteForce.experimento3.time
+      ], 
+      x: [
+        bruteForce.experimento1.charge, 
+        bruteForce.experimento2.charge, 
+        bruteForce.experimento3.charge
+      ],
       type: 'line',
-      name: 'Alg Brute Force'
+      fill: 'toself',
+      name: 'Brute Force'
     },
     {
-      y: [progDynamic.experimento1.time, progDynamic.experimento2.time, progDynamic.experimento3.time], 
-      x: [progDynamic.experimento1.charge, progDynamic.experimento2.charge, progDynamic.experimento3.charge],
+      y: [
+        progDynamic.experimento1.time, 
+        progDynamic.experimento2.time, 
+        progDynamic.experimento3.time
+      ], 
+      x: [
+        progDynamic.experimento1.charge, 
+        progDynamic.experimento2.charge, 
+        progDynamic.experimento3.charge
+      ],
       type: 'line',
-      name: 'Alg Dynamic Programming'
+      fill: 'toself',
+      name: 'Dynamic Programming'
     },
   ];
   
   return data
 }
 
-plotLib.plot(generateGraph());
+plotLib.plot(generateGraph(), [{
+  // polar: {
+  //   radialaxis: {
+  //     visible: true,
+  //     range: [0, 50]
+  //   }
+  // }
+}]);
